51afb3928ebec87d1dbc9cb1cc7316c146e0e8f1
MarieF118 <72809324+MarieF118@users.noreply.github.com>
2021-01-19 10:07:27 +0100

    Creer et utiliser l'objet computer

---
+++ classes/Computer.php
@@ -0,0 +1,19 @@
+<?php
+
+class Computer
+{
+    /**
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @var array
+     */
+    public $components;
+
+    /**
+     * @var array
+     */
+    public $devices;
+}
--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 <?php
+
+// permet aux classes d'être automatiquement chargées si elles ne sont pas encore définies
+
+require_once "includes/autoload.php";
+
+$computer1 = new Computer();
+$computer1->name = "AsusX5";
+$computer1->components = ["cpu, gpu, motherboard, ram"];
+$computer1->devices = ["mouse, keyboard, screen, webcam"];
+
+var_dump($computer1);
