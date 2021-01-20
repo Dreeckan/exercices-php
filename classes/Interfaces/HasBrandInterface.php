c943a885b887dd32ad26d8f6c36ea414f2d73c52
MarieF118 <72809324+MarieF118@users.noreply.github.com>
2021-01-20 10:42:10 +0100

    Update HasBrandInterface.php

--- classes/Interfaces/HasBrandInterface.php
+++ classes/Interfaces/HasBrandInterface.php
@@ -6,4 +6,5 @@ interface HasBrandInterface
 {
     public function getBrand(): ?string;
     public function setBrand(?string $name): HasBrandInterface;
+    //retourne n'importe quel objet qui implemente HasNameInterface
 }
