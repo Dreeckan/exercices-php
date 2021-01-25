97ed43b094d29be33bc19cae18dd429676a91da4
franckcode6 <franck.quasevi@yahoo.com>
2021-01-25 16:55:55 +0100

    Partie 4

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php
+<?php 
+
+/*$variable = $_GET['variable'];
+    if(isset($variable)) {
+        echo $variable;
+    } else {
+        echo $_GET['variable'];
+    }*/
+
+ function uneFonction (int $parametre): int
+    {
+        return $parametre * 2;
+    }
+
+uneFonction($parametre = 2);
+
