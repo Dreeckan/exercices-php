f2787c0efb850ce62f1c309b9367a3b0304361ae
vincentvdt <vincent.vidot3@gmail.com>
2021-01-25 14:31:30 +0100

    do revision ex 1

--- 1-html.php
+++ 1-html.php
@@ -27,3 +27,56 @@ $shoes = [
         'stock' => 32,
     ],
 ];
+
+function addPromo($prix, $reduc){
+    if ($prix <= 100 ) return;
+    return  number_format($prix * (1 - ($reduc / 100)),2,',', '') . '€';
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<table>
+    <thead>
+        <tr style='font-weight : bold'>
+            <td>Nom</td>
+            <td>Prix</td>
+            <td>Promo</td>
+            <td>Stock</td>
+            <td>Lien</td>
+        </tr>
+    </thead>
+    <tbody>
+
+        <?php 
+    foreach ($shoes as $shoe){
+        if ($shoe['stock'] < 20) {
+            $color = 'red';
+        } else {
+            $color = null;
+        }
+
+        echo '
+        <tr style="color : '.$color.'">
+            <td>' .$shoe['name'] . '</td>
+            <td>' .number_format($shoe['price'],2,',','') . '€</td>
+            <td>' .addPromo($shoe['price'], 10) . '</td>
+            <td>' .$shoe['stock'] . '</td>
+            <td><a href="2-superglobales.php?stock='.$shoe['stock'].'">Lien</a></td>
+        </tr>
+        ';
+    }?>
+    
+    </tbody>
+</table>
+</body>
+</html>
