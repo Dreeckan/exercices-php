9020dcae463508b1dd89c8e3cfa7798d997f5579
Rémi Jarjat <remi@dreeckan.net>
2021-01-25 18:09:02 +0100

    Exercice de révision numéro 1

--- 1-html.php
+++ 1-html.php
@@ -27,3 +27,60 @@ $shoes = [
         'stock' => 32,
     ],
 ];
+?>
+
+<table>
+    <tr>
+        <th>Nom</th>
+        <th>Prix</th>
+        <th>Prix avec réduction</th>
+        <th>Quantité</th>
+    </tr>
+    <?php
+        foreach ($shoes as $shoe) {
+            $color = null;
+            if ($shoe['stock'] < 20) {
+                $color = 'red';
+            }
+    ?>
+        <tr <?php if ($color) { echo 'style="color: '.$color.'"';}?>>
+            <td>
+                <a href="2-superglobales.php?stock=<?php echo $shoe['stock']; ?>">
+                    <?= $shoe['name']; ?>
+                </a>
+            </td>
+            <td>
+                <?= number_format($shoe['price'], 2, ',', ' '); ?>€
+            </td>
+            <td>
+                <?php
+                if ($shoe['price'] >= 100) {
+                    echo number_format(reduction($shoe['price'], 10), 2, ',', ' ').'€';
+                }
+                ?>
+            </td>
+            <td>
+                <?= $shoe['stock']; ?>
+            </td>
+        </tr>
+    <?php } ?>
+</table>
+
+<?php
+
+/**
+ * @param float $price
+ * @param float $tauxReduction
+ *
+ * @return float
+ */
+function reduction(float $price, float $tauxReduction): float
+{
+    // 1 + ($tauxReduction / 100) ne doit pas être égal à 0
+    if ($tauxReduction == -100) {
+        return 0.0;
+    }
+    $valeurReduction = 1 + ($tauxReduction / 100);
+
+    return $price / $valeurReduction;
+}
