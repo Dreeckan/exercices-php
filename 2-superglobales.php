582f12afa6ff03003bba7eab4f589ecd9bad8536
franckcode6 <franck.quasevi@yahoo.com>
2021-01-25 15:25:58 +0100

    Partie 2

--- 2-superglobales.php
+++ 2-superglobales.php
@@ -1,10 +1,52 @@
-<form action="">
+<?php
+    session_start();
+    var_dump( $_GET['stock']);
+
+    $error = false;
+    
+
+    if(isset($_GET['stock']) && empty( $_GET['stock']))
+    {
+        header('Location : 1-html.php');
+    } else {
+        $_SESSION['stock'] = $_GET['stock'];
+    }
+    ?>
+    
+    
+    <form action="" method="POST">
     <div>
-        <label for="test">Un champ de test</label>
-        <input type="text" id="test">
+        <label for="quantity">Quantité</label>
+        <input type="number" id="test" name="quantity">
     </div>
     <div>
-        <label for="email">Un champ email</label>
-        <input type="email" id="email">
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email">
     </div>
-</form>
+    <input type="submit">
+    </form>
+
+<?php
+    if(isset($_POST['quantity']) && $_POST['quantity'] > $_SESSION['stock'])
+    {   
+        $error = true;
+        echo '<div class="alert alert-primary col-md-6 mx-auto my-5" role="alert">
+                Il n y a pas assez de stock pour répondre à votre demande!
+                </div>';
+    }
+
+    if(isset($_POST['email']) && !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+    {
+        $error = true;
+        echo '<div class="alert alert-primary col-md-6 mx-auto my-5" role="alert">
+                Veuillez entrer un email valide!
+                </div>';
+    }
+
+    if(isset($_POST['quantity']) && isset($_POST['email']) && !$error)
+    {
+        $_SESSION['quantity'] = $_POST['quantity'];
+        $_SESSION['email'] = $_POST['email'];
+    }
+
+    var_dump($_SESSION);
