babbfede577855782dd307c1204a75ec670e139e
Gilman <antoine.gilman@live.fr>
2021-01-25 16:22:11 +0100

    partie2

--- 2-superglobales.php
+++ 2-superglobales.php
@@ -1,10 +1,44 @@
-<form action="">
+
+
+<?php
+session_start();
+
+if (empty($_GET['stock'])) {
+    header('Location: 1-html.php');
+} else {
+    $_SESSION['stock'] = $_GET['stock'];
+}
+
+
+if (isset($_POST['quantity'])) {
+    if ($_POST['quantity'] >= $_SESSION['stock']) {
+        echo "
+        <div>Il n'y a pas assez de stock pour répondre à votre demande🔮</div>";
+    }
+}
+
+if (isset($_POST['mail'])) {
+    if (filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL)==false) {
+        echo "<div>Email invalide🚽</div>";
+    }
+}
+
+?>
+
+<form method="post" action="">
     <div>
-        <label for="test">Un champ de test</label>
-        <input type="text" id="test">
+        <label for="quantity">Quantité</label>
+        <input type="number" name="quantity" id="test" min = "0" max="">
+
+
     </div>
     <div>
-        <label for="email">Un champ email</label>
-        <input type="email" id="email">
+        <label for="email">Email📧</label>
+        <input type="email" name='mail' id="email">
+    </div>
+    <div>
+         <button type="submit"> Envoyer </button>
     </div>
 </form>
+
+
