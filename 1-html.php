1cac35e3a392c9ed507a824d44642b7e0504d263
Larazzia <sigismeau.clement@gmail.com>
2021-01-25 15:44:23 +0100

    fin exo1

--- 1-html.php
+++ 1-html.php
@@ -1,5 +1,11 @@
 <?php
-
+function reduction($reduc)
+{
+    if ($reduc > 100) {
+        $remise = $reduc / 1.10;
+        return number_format($remise, 2, ',', ' ');
+    }
+}
 $shoes = [
     [
         'name'  => 'Heracles air max ++',
@@ -27,3 +33,48 @@ $shoes = [
         'stock' => 32,
     ],
 ];
+
+
+?>
+
+
+<table>
+    <thead>
+        <tr>
+            <?php
+            foreach ($shoes[0] as $key => $value) { ?>
+                <th><?= $key ?></th>
+            <?php } ?>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        foreach ($shoes as $value) {
+
+            /**
+             * les variables recupères et stock les valeurs du tableau shoes.
+             */
+            $name = $value['name'];
+            $price = $value['price'];
+            $stock = $value['stock'];
+        ?>
+            <tr <?php if ($stock < 20) {
+                ?> style="color:#ff0000" ; <?php
+                                        } ?>>
+                <td><?php echo $name ?></td>
+                <td><?php echo $price;
+                    echo "€";
+                    if ($price > 100) {
+                        echo " soldé à :";
+                        echo reduction($price);
+                        echo "€";
+                    }
+                    ?></td>
+                <td><?php echo $stock ?></td>
+                <td><a href="2-superglobales.php?stock=<?php echo $stock; ?>">Acheter</a></td>
+            </tr>
+        <?php
+        }
+        ?>
+    </tbody>
+</table>
