dc0f0f2e39632d2d5f00af09af6ccad8ca0ecdb3
geoffrey <biggeo22@hotmail.fr>
2021-01-25 16:02:49 +0100

    Fin de l'exercice 1 en 10 jours au lieu de 15 min

--- 1-html.php
+++ 1-html.php
@@ -26,4 +26,38 @@ $shoes = [
         'price' => 55.12,
         'stock' => 32,
     ],
-];
+]; ?>
+
+<table>
+    <?php foreach ($shoes as $shoe) {
+        if ($shoe['stock'] <= 20) {
+            $color = "red";
+        } else {
+            $color = "blue";
+        }
+
+    ?>
+        <tr style="color: <?php echo $color; ?>">
+            <td> <?php echo $shoe['name']; ?>
+            </td>
+            <td> <?php echo $shoe['price'];
+                    if ($shoe['price'] > 100) {
+                        echo " soldé à ";
+                        echo addpromo($shoe['price']);
+                        echo "€";
+                    } ?> </td>
+            <td> <?php echo $shoe['stock']; ?> </td>
+            <td> <a href="2-superglobales.php?stock=<?php echo $shoe['stock']; ?>"> Ajouter </a> </td>
+
+        </tr>
+
+    <?php } ?>
+</table>
+
+
+<?php
+function addpromo($item)
+{
+    $prixRemise = $item / 1.10;
+    return number_format($prixRemise, 2, ',', ' ');
+}
