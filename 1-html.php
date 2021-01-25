f83d0dc81aa57184f796257e2bcdbf1c8d22f9a5
franckcode6 <franck.quasevi@yahoo.com>
2021-01-25 14:20:31 +0100

    Partie 1

--- 1-html.php
+++ 1-html.php
@@ -1,5 +1,12 @@
 <?php
 
+function sale($price, $tauxReduction)
+{
+    $salePrice = $price - ($price * $tauxReduction/100);
+
+    return $salePrice;
+}
+
 $shoes = [
     [
         'name'  => 'Heracles air max ++',
@@ -27,3 +34,45 @@ $shoes = [
         'stock' => 32,
     ],
 ];
+
+?>
+
+<table>
+    <tr>
+        <th>Nom</th>
+        <th>Prix (en euros)</th>
+        <th>Nombre en stock</th>
+        <th>Prix en réduction (en euros)</th>
+    </tr>
+    <?php
+            
+
+        foreach ($shoes as $shoe)
+        {
+            if ($shoe['stock'] <= 20)
+            {
+                $colorStock = "red";
+            } else
+            {
+            $colorStock = "black";
+            }
+
+            if ($shoe['price'] > 100)
+            {
+                sale($shoe['price'], 10);
+            }
+
+            echo '<tr style = "color: '.$colorStock.'">
+                    <td><a href ="2-superglobales.php?stock='.$shoe['stock'].'">'.$shoe['name'].'</a></td>
+                    <td>'.number_format($shoe['price'], 2, ',', ' ').'€</td>
+                    <td >'.$shoe['stock'].' en stock</td>';
+
+                    if ($shoe['price'] > 100)
+                    {
+                        echo '<td> '.number_format(sale($shoe['price'],10), 2, ',', ' ').'€</td>';
+                    }
+                    
+                 '</tr>';
+        }
+    ?>
+</table>
