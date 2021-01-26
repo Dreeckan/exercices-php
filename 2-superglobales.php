c9c39a65c79ce4a58774ce1013968ce8d4734fb3
hardkos77 <62692670+hardkos77@users.noreply.github.com>
2021-01-26 11:03:49 +0100

    JLH-exo2

--- 2-superglobales.php
+++ 2-superglobales.php
@@ -1,23 +1,46 @@
 <?php
-  
-$_GET['stock'];
-
 echo $_GET['stock'];
-
 if (empty($_GET['stock'])) {
     header("location: 1-html.php");
 }
-
 ?>
 
-<form action="">
-    <div>
-        <label for="test">Un champ de test</label>
-        <input type="text" id="test">
-    </div>
+/********************** 
+* FORM HTML
+**********************/
+
+<form action="" method="POST">
     <div>
         <label for="email">Un champ email</label>
         <input type="email" id="email">
     </div>
+    <label for="quantity">Number</label>
+    <input type="number" id="quantity" name="quantity"
+       min="1" max=<?php echo $_GET['stock'] ?> ><br>
+       <input type="submit" value="Envoyer le formulaire">
 </form>
 
+<?php
+
+if (!empty(['stock'])) {
+    $commande = trim($_GET['stock']);
+} else {
+    echo 'Il n\'y a pas assez de stock pour répondre à votre demande';
+}
+
+if (isset($_POST['email'])) {
+    $email = trim($_POST['email']);
+    if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'Veuillez entrer un email valide';
+    }
+}
+
+session_start();
+if ($error != 1) {
+    if (isset($_POST['mail']) && isset($_POST['stock'])) {
+        $_SESSION['mail'] = $mailValide;
+        $_SESSION['commande'] = $commandeValide;
+    }
+}
+
+?>
